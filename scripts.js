3a3527cd99f497dddbd0ba83efa27de66cb85931
sabrina <93158967+SabrinaR0drigues@users.noreply.github.com>
2025-03-27 22:16:25 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// document.addEventListener("DOMContentLoaded", function (){
+ 
+//      document.getElementById("webBotao").addEventListener("click",function(){
+
+   
+//          alert("clicou")
+//      })
+
+//  });
+
+$(document).ready(function(){
+
+    $("#webBotao").click(function(){
+
+        alert("clicou")
+    });
+
+
+
+});
